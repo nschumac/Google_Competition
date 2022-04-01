6862efd37e4b5c53acec90d2621f536743f8b613
Nikolai Schumacher <nikolai@Nikolais-MacBook-Pro.local>
2022-04-01 22:50:02 +0200

    improved setup.py and added algorithm to includes

--- setup.py
+++ setup.py
@@ -5,31 +5,38 @@ import os
 import shutil
 
 def main():
-    sys.path.append(os.path.realpath('..'))
-    if sys.argv.__len__() != 4:
-        sys.stdout = sys.stderr
-        print("Usage: <year> <round> <amount>")
-        sys.exit(1)
-    else:
-        os.path.dirname(os.path.abspath(__file__))
-        path = ""
-        path += "./"
-        path += sys.argv[1]
-        path += "/round_"
-        path += sys.argv[2]
-        if os.path.exists(path):
-            sys.stdout = sys.stderr
-            print("clear directory before you execute")
-            sys.exit(1)
-        else:
-            os.mkdir(path)
-            path += "/"
-            for i in range (int(sys.argv[3])):
-                newfile = open(path + str(chr(65 + i)) + ".cpp", 'w')
-                shutil.copy("./default.cpp", path + str(chr(65 + i)) + ".cpp")
-    
+	sys.path.append(os.path.realpath('..'))
+	if sys.argv.__len__() != 5 and sys.argv.__len__() != 6:
+		sys.stdout = sys.stderr
+		print("Usage: <type k = Kickstart, c = Codejam> <year> <round> <amount> <optional -f for force>")
+		sys.exit(1)
+	else:
+		os.path.dirname(os.path.abspath(__file__))
+		path = ""
+		path += "./"
+		path += sys.argv[2]
+		path +=  "/kickstart" if sys.argv[1] == "k" else "/codejam"
+		path += "/round_"
+		path += sys.argv[3]
+		if not os.path.exists(path):
+			try:
+				os.makedirs(path)
+				print("Created Directory!")
+			except OSError as error:
+				print(error)
+		else:
+			print("Directory Exits!")
+		path += "/"
+		for i in range (int(sys.argv[4])):
+			newfile = path + str(chr(65 + i)) + ".cpp"
+			if not os.path.exists(newfile) or (sys.argv.__len__() == 6 and sys.argv[5] == "-f"):
+				shutil.copy("./default.cpp", newfile)
+				print("Created File " + newfile)
+			else:
+				print("File " + path + str(chr(65 + i)) + ".cpp exits already!")
+
 if __name__ == "__main__":
-    main()
+	main()
 
 
 
